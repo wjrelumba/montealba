6452cd1b7233826f0ed44bda828e9bef23436c1d
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2025-10-03 15:59:13 +0800

    fix: supabase error

--- src/contextProviders/UserProvider.jsx
+++ src/contextProviders/UserProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import supabase from "../supabase/Supabase";
+import supabase from "../supabase/supabase";
 
 const UserContext = createContext();
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useMedia } from '../../contextProviders/MediaProvider'
-import supabase from '../../supabase/Supabase';
+import supabase from '../../supabase/supabase';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../../contextProviders/UserProvider';
