3ee4b2a9dafefcb1af77d12da4e94b1972d73aca
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2025-10-14 10:40:39 +0800

    fix: create order page now checks if signed in

--- src/pages/Dashboard/CreateOrder/CreateOrder.jsx
+++ src/pages/Dashboard/CreateOrder/CreateOrder.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useUser } from '../../../contextProviders/UserProvider';
 
 export default function CreateOrder() {
+  const {signedIn} = useUser();
+  
+  useEffect(() => {
+          if(!signedIn){
+              navigate('/');
+          };
+      },[signedIn]);
   return (
     <div>CreateOrder</div>
   )
