bb7ee2d81bc919cd02ea9ae9949c988813d0b2cf
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2025-10-14 10:42:46 +0800

    fix: useNavigate missing in create orders page now added

--- src/pages/Dashboard/CreateOrder/CreateOrder.jsx
+++ src/pages/Dashboard/CreateOrder/CreateOrder.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
 import { useUser } from '../../../contextProviders/UserProvider';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateOrder() {
   const {signedIn} = useUser();
+  const navigate = useNavigate();
   
   useEffect(() => {
           if(!signedIn){
